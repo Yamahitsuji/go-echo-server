d464953ff86d22330c0e5c94831d5fe41c824ffb
Yamahitsuji <goat06131004@gmail.com>
2022-08-08 10:58:28 +0900

    add echo server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Yamahitsuji/go-echo-server
+
+go 1.18
---
+++ server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		var p any
+		err := json.NewDecoder(r.Body).Decode(&p)
+		if err != nil {
+			if err.Error() == "EOF" {
+				fmt.Fprint(w)
+				return
+			}
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		switch p.(type) {
+		case map[string]any:
+			jsonStr, err := json.Marshal(p)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			w.Header().Add("content-type", "application/json")
+			fmt.Fprint(w, string(jsonStr))
+		default:
+			fmt.Fprint(w)
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		host, err := os.Hostname()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w, "Hello World from %s", host)
+	})
+
+	fmt.Println("Starting server at port 80")
+	if err := http.ListenAndServe(":80", nil); err != nil {
+		log.Fatal(err)
+	}
+}
